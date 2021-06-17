f077d90b9bee5f4f98d87f3a5f5e2bee2ca46aff
pkgit-0 <kell108p@protonmail.com>
2021-06-17 06:42:17 -0400

    commit -m 99

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'learning_logs',
+    'users',
+
+    # Third party app
+    'bootstrap4',
 
     # Default django apps
     'django.contrib.admin',
@@ -127,3 +131,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# My settings
+LOGIN_URL = 'users:login'
